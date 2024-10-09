542b1c35c0e25416818eba4de1067af94d90c17b
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-10-09 16:02:21 +0530

    App Setup, Added navigation and screens along with redux toolkit basic setup

---
+++ src/constants/Colors.ts
@@ -0,0 +1,8 @@
+export const colors = {
+    red: '#DF201F',
+    green: '#94CD00',
+    black: '#161A1D',
+    white: '#fff',
+    textColor: '#99959E',
+    lightTextColor: '#A2A3A5',
+  };
---
+++ src/navigations/StackNavigation.tsx
@@ -0,0 +1,55 @@
+import React, {useEffect, useState} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import SplashScreen from '../screens/SplashScreen';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+
+const Stack = createNativeStackNavigator();
+
+const StackNavigation = () => {
+  const [isActive, setIsActive] = useState<boolean>(false);
+
+  const splashScreenHandler = () => {
+    setIsActive(!isActive);
+  };
+  useEffect(() => {
+    setTimeout(() => {
+      splashScreenHandler();
+    }, 3000);
+  },[]);
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {isActive ? (
+          <Stack.Screen
+            options={{headerShown: false}}
+            name="splashScreen"
+            component={SplashScreen}
+          />
+        ) : (
+          <>
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="onboardingScreen"
+              component={OnboardingScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="loginScreen"
+              component={LoginScreen}
+            />
+            <Stack.Screen
+              options={{headerShown: false}}
+              name="registerScreen"
+              component={RegisterScreen}
+            />
+          </>
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default StackNavigation;
