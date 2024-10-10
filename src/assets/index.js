d9c29b8b41711084f52bf9598decb440115f8fc0
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-10-10 12:17:16 +0530

    Added Onboarding Screen

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,1 +1,9 @@
 export const splashBg = require('./images/Splash.png');
+export const onboardingEllipse = require('./images/OnboardingEllipse.png');
+export const carouselOne = require('./images/Carousal1.jpg');
+export const carouselTwo = require('./images/Carousal2.jpg');
+export const carouselThree = require('./images/Carousal3.jpg');
+export const carouselFour = require('./images/Carousal4.jpg');
+
+
+
