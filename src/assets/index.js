19ebacb3fd0ea543cbf9365acea9764ac18cf062
Tanshu Allewar <tanshuallewar98@gmail.com>
2024-10-11 13:06:19 +0530

    Added Login Screen, Country codes, Status bar

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,6 +4,15 @@ export const carouselOne = require('./images/Carousal1.jpg');
 export const carouselTwo = require('./images/Carousal2.jpg');
 export const carouselThree = require('./images/Carousal3.jpg');
 export const carouselFour = require('./images/Carousal4.jpg');
+export const loginbgOne = require('./images/Loginbg1.png');
+export const loginbgTwo = require('./images/Loginbg2.png');
+export const loginStoreIcon = require('./images/store.png');
+export const customerIconWhite = require('./images/CustomerWhiteBg.png');
+export const customerIconRed = require('./images/CustomerRedBg.png');
+
+export const sellerIconStore = require('./images/SellerIcon.png');
+
+
 
 
 
